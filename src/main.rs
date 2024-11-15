25df7b9174f564725f1068a0c10fdf82067b570f
Viktor Albert <albertviktor@utitech.ro>
2024-11-15 15:34:00 +0200

    Added decoder

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::io::{self, Write, Read};
+use std::io::{Write, Read};
 use std::time::Duration;
 use std::{thread, vec};
 use serialport::SerialPort;
@@ -7,9 +7,9 @@ use phf::phf_map;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let poll = "R,33\r\n";
-    let initialize_commands = vec!["M,1\r\n", "R,30\r\n","R,34,FFFF0000\r\n"];
+    let initialize_commands = vec!["M,1\r\n", "R,30\r\n", "R,34,FFFF0000\r\n"];
 
-    let mut port = serialport::new("dev/ttyAMA0", 115200)
+    let mut port = serialport::new("/dev/ttyAMA0", 115200)
         .timeout(Duration::from_millis(5000))
         .open()?;
     println!("Opened serial port successfully.");
@@ -19,55 +19,54 @@ fn main() -> Result<(), Box<dyn Error>> {
         port.flush()?;
     }
 
-    let mut response_buf: Vec<u8> = vec![0; 32];
 
     loop {
         port.write_all(poll.as_bytes())?;
         port.flush()?;
 
-        let read_buf = port.read(response_buf.as_mut_slice())?;
-        decoder(&read_buf, &port);
-
+        let mut read_buf = vec![0; 32];
+        let read_len = port.read(&mut read_buf)?;
+        decoder(&read_buf[..read_len], &mut *port);
 
         thread::sleep(Duration::from_millis(500));
     }
 }
 
-fn decoder(byte_array: &usize, serial: &Box<dyn SerialPort>) {
-    let bytes = byte_array.as_bytes().chunks(2).map(|chunk| chunk[0] as usize).collect();
+fn decoder(byte_array: &[u8], serial: &mut dyn SerialPort) {
+    let bytes = byte_array.chunks(2).map(|chunk| chunk[0] as usize).collect::<Vec<_>>();
 
     for byte in bytes {
-        execute_command(byte, &serial);
+        execute_command(byte, serial);
     }
 }
 
-fn execute_command(byte_command: ByteCommand, mut serial: Box<dyn SerialPort>) {
-    match byte_command {
-        ByteCommand::OneRonStacked => {
+fn execute_command(byte_command: usize, serial: &mut dyn SerialPort) {
+    match BYTE_TABLE.get(&byte_command.to_string().as_str()) {
+        Some(ByteCommand::OneRonStacked) => {
             serial.write_all("R,13,0500640001\r\n".as_bytes()).unwrap();
             thread::sleep(Duration::from_millis(100));
             serial.write_all("R,14,01\r\n".as_bytes()).unwrap();
         }
-        ByteCommand::FiveRonStacked => {
+        Some(ByteCommand::FiveRonStacked) => {
             serial.write_all("R,13,0501F40002\r\n".as_bytes()).unwrap();
             thread::sleep(Duration::from_millis(100));
             serial.write_all("R,14,01\r\n".as_bytes()).unwrap();
-
         }
-        ByteCommand::TenRonStacked => {
+        Some(ByteCommand::TenRonStacked) => {
             serial.write_all("R,13,0527100003\r\n".as_bytes()).unwrap();
             thread::sleep(Duration::from_millis(100));
             serial.write_all("R,14,01\r\n".as_bytes()).unwrap();
-
         }
-        ByteCommand::FiftyRonStacked => {
+        Some(ByteCommand::FiftyRonStacked) => {
             serial.write_all("R,13,05C3500004\r\n".as_bytes()).unwrap();
             thread::sleep(Duration::from_millis(100));
             serial.write_all("R,14,01\r\n".as_bytes()).unwrap();
         }
+        _ => {}
     }
 }
 
+#[derive(PartialEq, Eq, Hash)]
 enum ByteCommand {
     OneRonStacked,
     FiveRonStacked,
@@ -75,7 +74,7 @@ enum ByteCommand {
     FiftyRonStacked,
 }
 
-static BYTE_TABLE: phf::Map<&'static str, ByteCommand>= phf_map! {
+static BYTE_TABLE: phf::Map<&'static str, ByteCommand> = phf_map! {
     "80" => ByteCommand::OneRonStacked,
     "81" => ByteCommand::FiveRonStacked,
     "82" => ByteCommand::TenRonStacked,
